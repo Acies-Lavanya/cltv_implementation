9b2e0efc60e414ec9d176d8daa7d5be40e18a39c
Jeffrey Daniel <jeffrey.daniel@aciesglobal.com>
2025-06-30 12:07:38 +0530

    Delete cltv_implementation directory

--- cltv_implementation/CLTV_Complete/utils/file_input.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-def handle_file_uploads():
-    orders_file = st.file_uploader("Upload Orders CSV", type=["csv"], key="orders_upload")
-    transactions_file = st.file_uploader("Upload Transactions CSV", type=["csv"], key="transactions_upload")
-    if orders_file and transactions_file:
-        return pd.read_csv(orders_file), pd.read_csv(transactions_file)
-    return None, None
